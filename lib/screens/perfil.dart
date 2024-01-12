b960fdba944a0319f45bdfdee3de4e123a6efa92
Federico <federico_h_23@hotmail.com>
2024-01-11 21:24:28 -0500

    added json read, todo: fix perfil

--- lib/screens/perfil.dart
+++ lib/screens/perfil.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as rootBundle;
+
+import '../providers/provider_product.dart';
 
 class PerfilScreen extends StatelessWidget {
   const PerfilScreen({super.key});
@@ -93,7 +98,82 @@ class PerfilScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: const Center(
+        body: FutureBuilder(
+          future: ReadJsonData(),
+          builder: (context, data) {
+
+            if (data.hasError) {
+              return Center(child: Text("${data.error}"));
+            } else if (data.hasData) {
+              var items = data.data as List<ProductDataModel>;
+
+              return ListView.builder(
+                  itemCount: items == null ? 0 : items.length,
+                  itemBuilder: (context, index) {
+
+                    return
+
+                      Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 6),
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 80,
+                              height: 50,
+                              child: Image(
+                                image: NetworkImage(
+                                    items[index].imageURL.toString()),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 20, right: 8),
+                                      child: Text(
+                                        items[index].name.toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 20, right: 8),
+                                      child: Text(
+                                          items[index].oldPrice.toString()),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
+  }
+
+/*
             child: Column(children: [
           SizedBox(
             height: 20,
@@ -104,6 +184,15 @@ class PerfilScreen extends StatelessWidget {
               Shadow(blurRadius: 20, color: Colors.blue, offset: Offset(2, 2))
             ]),
           ),
-        ])));
-  }
+        ])),
+
+*/
+}
+
+Future<List<ProductDataModel>> ReadJsonData() async {
+  final jsondata =
+      await rootBundle.rootBundle.loadString('data/productos.json');
+  final list = json.decode(jsondata) as List<dynamic>;
+
+  return list.map((e) => ProductDataModel.fromJson(e)).toList();
 }
